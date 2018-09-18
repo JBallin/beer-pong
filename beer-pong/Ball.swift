47e82eb4b4bbda5b070868554f3408077548a74e
JBallin <JBallin@users.noreply.github.com>
2018-09-18 15:01:44 -0700

    Color ball slightly transparent orange

--- beer-pong/Ball.swift
+++ beer-pong/Ball.swift
@@ -8,6 +8,7 @@ class Ball: SCNNode {
         self.geometry = SCNSphere(radius: 0.02)
         let shape = SCNPhysicsShape(geometry: SCNSphere(radius: 0.02))
         self.physicsBody = SCNPhysicsBody(type: .dynamic, shape: shape)
+        self.geometry?.firstMaterial?.diffuse.contents = UIColor(red: 1.0, green: 0.6, blue: 0.2, alpha: 0.995)
     }
     
     required init?(coder decoder: NSCoder) {
